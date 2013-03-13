d3335b035d35e6a66a5327549fa64e628b79a4e4
iarielduarte <iariel.javauser@gmail.com>
2013-03-13 10:47:49 +0100

    Update master

--- com.phonegap.app2_Android/gen/R.java.d
+++ com.phonegap.app2_Android/gen/R.java.d
@@ -7,4 +7,10 @@ C:\MOBILE~2\COMPHO~1.APP\res\drawable-mdpi\icon.png \
 C:\MOBILE~2\COMPHO~1.APP\res\drawable-mdpi\splash.png \
 C:\MOBILE~2\COMPHO~1.APP\res\layout\main.xml \
 C:\MOBILE~2\COMPHO~1.APP\res\values\strings.xml \
+C:\MOBILE~2\COMPHO~1.APP\bin\res\drawable-hdpi\icon.png \
+C:\MOBILE~2\COMPHO~1.APP\bin\res\drawable-hdpi\splash.png \
+C:\MOBILE~2\COMPHO~1.APP\bin\res\drawable-ldpi\icon.png \
+C:\MOBILE~2\COMPHO~1.APP\bin\res\drawable-ldpi\splash.png \
+C:\MOBILE~2\COMPHO~1.APP\bin\res\drawable-mdpi\icon.png \
+C:\MOBILE~2\COMPHO~1.APP\bin\res\drawable-mdpi\splash.png \
 C:\MOBILE~2\COMPHO~1.APP\bin\AndroidManifest.xml \
