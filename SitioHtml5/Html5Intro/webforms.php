e61424c272544dc5dfb2e9a2dff4c7186475b80b
iarielduarte <iariel.javauser@gmail.com>
2013-03-15 18:08:19 +0100

    Update master

---
+++ SitioHtml5/Html5Intro/webforms.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Formulario de Respuesta</title>
+</head>
+
+<body>
+<h1>Leer datos de controles HTML5</h1>
+Has enviado
+<?php
+	echo $_REQUEST["localfechahora"];
+?>
+</body>
+</html>
